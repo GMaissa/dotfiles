d1f46a654246388a22c7504510a7d9f00c8f12e9
GMaissa <guillaume@maissa.fr>
2014-04-01 13:32:54 +0200

    reorganize files

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 #aptitude install 'git zsh tmux vim'
 
@@ -12,13 +12,20 @@ WARN='\033[0;33m'
 ERROR='\033[1;31m'
 DEFAULT='\033[0m'
 
-if [ ! type "git" > /dev/null ]; then
-	echo ${ERROR}"You need git to be installed to perform the setup"${DEFAULT}
+if ! type "git" > /dev/null; then
+	echo ${ERROR}"\nYou need git to be installed to perform the setup\n"${DEFAULT}
 	exit 1
 fi
-
-if [ ! -f /bin/zsh ]; then
-	echo ${ERROR}"ZSH needs to be installed"${DEFAULT}
+if ! type "zsh" > /dev/null; then
+	echo ${ERROR}"\nZSH needs to be installed\n"${DEFAULT}
+	exit 1
+fi
+if ! type "tmux" > /dev/null; then
+	echo ${ERROR}"\nTMUX needs to be installed\n"${DEFAULT}
+	exit 1
+fi
+if ! type "vim" > /dev/null; then
+	echo ${ERROR}"\nVIM needs to be installed\n"${DEFAULT}
 	exit 1
 fi
 
@@ -26,39 +33,39 @@ CUR_PATH=$(pwd)
 
 echo ${INFO}"Cloning OH MY ZSH repo ..."${DEFAULT}
 git clone ${OHMYZSHREPO} ~/.oh-my-zsh
-echo ${OK}'Done.'${DEFAULT}
+echo ${OK}"Done.\n"${DEFAULT}
 
 echo ${INFO}"Adding ZSH configuration ..."${DEFAULT}
 if [ -f ~/.zshrc ]; then
 	echo ${WARN}"Moving old config to ~/.zshrc.bak"${DEFAULT}
     mv ~/.zshrc ~/.zshrc.bak
 fi
-ln -s ${CUR_PATH}/zshrc ~/.zshrc
-echo ${OK}'Done.'${DEFAULT}
+ln -s ${CUR_PATH}/config/zsh ~/.zshrc
+echo ${OK}"Done.\n"${DEFAULT}
 
 echo ${INFO}"Adding command aliases ..."${DEFAULT}
 if [ -f ~/.aliases ]; then
 	echo ${WARN}"Moving old config to ~/.aliases.bak"${DEFAULT}
     mv ~/.aliases ~/.aliases.bak
 fi
-ln -s ${CUR_PATH}/aliases ~/.aliases
-echo ${OK}'Done.'${DEFAULT}
+ln -s ${CUR_PATH}/config/aliases ~/.aliases
+echo ${OK}"Done.\n"${DEFAULT}
 
 echo ${INFO}"Adding TMUX configuration ..."${DEFAULT}
 if [ -f ~/.tmux.conf ]; then
 	echo ${WARN}"Moving old config to ~/.tmux.conf.bak"${DEFAULT}
     mv ~/.tmux.conf ~/.tmux.conf.bak
 fi
-ln -s ${CUR_PATH}/tmux.conf ~/.tmux.conf
-echo ${OK}'Done.'${DEFAULT}
+ln -s ${CUR_PATH}/config/tmux ~/.tmux.conf
+echo ${OK}"Done.\n"${DEFAULT}
 
 echo ${INFO}"Adding VIM configuration ..."${DEFAULT}
 if [ -f ~/.vimrc ];then
 	echo ${WARN}"Moving old config to ~/.vimrc.bak"${DEFAULT}
     mv ~/.vimrc ~/.vimrc.bak
 fi
-ln -s ${CUR_PATH}/vimrc ~/.vimrc
-echo ${OK}'Done.'${DEFAULT}
+ln -s ${CUR_PATH}/config/vim ~/.vimrc
+echo ${OK}"Done.\n"${DEFAULT}
 
 echo ${INFO}"Adding SSH configuration ..."${DEFAULT}
 if [ ! -d ~/.ssh ];then
@@ -67,13 +74,13 @@ elif [ -f ~/.ssh/config ];then
 	echo ${WARN}"Moving old config to ~/.ssh/config.bak"${DEFAULT}
     mv ~/.ssh/config ~/.ssh/config.bak
 fi
-ln -s ${CUR_PATH}/ssh.conf ~/.ssh/config
-echo ${OK}'Done.'${DEFAULT}
+ln -s ${CUR_PATH}/config/ssh ~/.ssh/config
+echo ${OK}"Done.\n"${DEFAULT}
 
 echo ${INFO}"Adding GIT configuration ..."${DEFAULT}
 if [ -f ~/.gitconfig ];then
 	echo ${WARN}"Moving old config to ~/.gitconfig.bak"${DEFAULT}
     mv ~/.gitconfig ~/.gitconfig.bak
 fi
-ln -s ${CUR_PATH}/gitconfig ~/.gitconfig
-echo ${OK}'Done.'${DEFAULT}
+ln -s ${CUR_PATH}/config/git ~/.gitconfig
+echo ${OK}"Done.\n"${DEFAULT}
