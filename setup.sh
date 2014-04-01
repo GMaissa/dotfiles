1e2cb135961c5f0b410fea5cc80e212359a259a3
GMaissa <guillaume@maissa.fr>
2014-04-01 14:31:38 +0200

    update ssh configuration management

--- setup.sh
+++ setup.sh
@@ -12,21 +12,17 @@ WARN='\033[0;33m'
 ERROR='\033[1;31m'
 DEFAULT='\033[0m'
 
-if ! type "git" > /dev/null; then
-	echo ${ERROR}"\nYou need git to be installed to perform the setup\n"${DEFAULT}
-	exit 1
-fi
 if ! type "zsh" > /dev/null; then
-	echo ${ERROR}"\nZSH needs to be installed\n"${DEFAULT}
-	exit 1
+    echo ${ERROR}"\nZSH needs to be installed\n"${DEFAULT}
+    exit 1
 fi
 if ! type "tmux" > /dev/null; then
-	echo ${ERROR}"\nTMUX needs to be installed\n"${DEFAULT}
-	exit 1
+    echo ${ERROR}"\nTMUX needs to be installed\n"${DEFAULT}
+    exit 1
 fi
 if ! type "vim" > /dev/null; then
-	echo ${ERROR}"\nVIM needs to be installed\n"${DEFAULT}
-	exit 1
+    echo ${ERROR}"\nVIM needs to be installed\n"${DEFAULT}
+    exit 1
 fi
 
 CUR_PATH=$(pwd)
@@ -35,52 +31,42 @@ echo ${INFO}"Cloning OH MY ZSH repo ..."${DEFAULT}
 git clone ${OHMYZSHREPO} ~/.oh-my-zsh
 echo ${OK}"Done.\n"${DEFAULT}
 
-echo ${INFO}"Adding ZSH configuration ..."${DEFAULT}
-if [ -f ~/.zshrc ]; then
-	echo ${WARN}"Moving old config to ~/.zshrc.bak"${DEFAULT}
-    mv ~/.zshrc ~/.zshrc.bak
-fi
-ln -s ${CUR_PATH}/config/zsh ~/.zshrc
-echo ${OK}"Done.\n"${DEFAULT}
+symlinkConfig "zsh" ".zshrc"
 
-echo ${INFO}"Adding command aliases ..."${DEFAULT}
-if [ -f ~/.aliases ]; then
-	echo ${WARN}"Moving old config to ~/.aliases.bak"${DEFAULT}
-    mv ~/.aliases ~/.aliases.bak
-fi
-ln -s ${CUR_PATH}/config/aliases ~/.aliases
-echo ${OK}"Done.\n"${DEFAULT}
+symlinkConfig "aliases" ".aliases"
 
-echo ${INFO}"Adding TMUX configuration ..."${DEFAULT}
-if [ -f ~/.tmux.conf ]; then
-	echo ${WARN}"Moving old config to ~/.tmux.conf.bak"${DEFAULT}
-    mv ~/.tmux.conf ~/.tmux.conf.bak
-fi
-ln -s ${CUR_PATH}/config/tmux ~/.tmux.conf
-echo ${OK}"Done.\n"${DEFAULT}
+symlinkConfig "tmux" ".tmux.conf"
 
-echo ${INFO}"Adding VIM configuration ..."${DEFAULT}
-if [ -f ~/.vimrc ];then
-	echo ${WARN}"Moving old config to ~/.vimrc.bak"${DEFAULT}
-    mv ~/.vimrc ~/.vimrc.bak
+symlinkConfig "vim" ".vimrc"
+if [ ! -d ~/.vim/backup ]; then
+	mkdir -p ~/.vim/backup
 fi
-ln -s ${CUR_PATH}/config/vim ~/.vimrc
-echo ${OK}"Done.\n"${DEFAULT}
 
-echo ${INFO}"Adding SSH configuration ..."${DEFAULT}
+symlinkConfig "git" ".gitconfig"
+
 if [ ! -d ~/.ssh ];then
     mkdir ~/.ssh
-elif [ -f ~/.ssh/config ];then
-	echo ${WARN}"Moving old config to ~/.ssh/config.bak"${DEFAULT}
-    mv ~/.ssh/config ~/.ssh/config.bak
-fi
-ln -s ${CUR_PATH}/config/ssh ~/.ssh/config
-echo ${OK}"Done.\n"${DEFAULT}
+if
+symlinkConfig "ssh" ".ssh/config"
 
-echo ${INFO}"Adding GIT configuration ..."${DEFAULT}
-if [ -f ~/.gitconfig ];then
-	echo ${WARN}"Moving old config to ~/.gitconfig.bak"${DEFAULT}
-    mv ~/.gitconfig ~/.gitconfig.bak
+if [ ! -d ~/bin ]; then
+	mkdir ~/bin
 fi
-ln -s ${CUR_PATH}/config/git ~/.gitconfig
-echo ${OK}"Done.\n"${DEFAULT}
+ln -s ${CUR_PATH}/bin/ssh ~/bin/ssh
+ln -s ${CUR_PATH}/bin/ssh-copy-id ~/bin/ssh-copy-id
+
+symlinkConfig()
+{
+	CONFFILE=$1
+	SYMLINKNAME=$2
+    echo ${INFO}"Adding ${CONFFILE} configuration ..."${DEFAULT}
+    if [ -f ~/${SYMLINKNAME} ];then
+        echo ${WARN}"Moving old config to ~/${SYMLINKNAME}.bak"${DEFAULT}
+        mv ~/${SYMLINKNAME} ~/${SYMLINKNAME}.bak
+    elif [ -L ~/${SYMLINKNAME} ];then
+        echo ${WARN}"Removing old symlinked config ~/${SYMLINKNAME}"${DEFAULT}
+    	rm ~/${SYMLINKNAME}
+    fi
+    ln -s ${CUR_PATH}/config/${CONFFILE} ~/${SYMLINKNAME}
+    echo ${OK}"Done.\n"${DEFAULT}
+}
