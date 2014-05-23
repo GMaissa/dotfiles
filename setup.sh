6cd1c0437ca0dfb08a155380734ab9034e9db846
Guillaume Maïssa <guillaume@maissa.fr>
2014-05-23 18:14:02 +0200

    split os specific setup commands into dedicated files

--- setup.sh
+++ setup.sh
@@ -172,23 +172,8 @@ if [ $# -eq 1 -a "$1" = "with-ssh" ]; then
     symlink_config "ssh" ".ssh/config"
 fi
 
-if [[ "${OS}" -eq "debian" ]];then
-    STEPMSG=$(printf "%-45s" "Applying Solarized theme")
-    echo -ne "${STEPMSG}${PROCESSMSG}"\\r
-    if [[ ! -f ${HOME}/.dircolors ]];then
-        wget --no-check-certificate https://raw.github.com/seebi/dircolors-solarized/master/dircolors.ansi-dark ${HOME}/.dircolors >/dev/null 2>&1
-    fi
-    if [[ ! -d ${HOME}/.solarized-terminal ]]; then
-        git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git ${HOME}/.solarized-terminal >/dev/null 2>&1
-    fi
-    OUTPUT=$(${HOME}/.solarized-terminal/set_dark.sh 2>&1 >/dev/null)
-    if [ $? -ne 0 ]; then
-        echo -e "${STEPMSG}${ERRORMSG}"
-        echo -e ${OUTPUT}
-        exit 1
-    fi
-    echo -e "${STEPMSG}${SUCCESSMSG}"
-    
+if [ -f "$(dirname $0)/setup-${OS}.sh" ];then
+    . $(dirname $0)/setup-${OS}.sh
 fi
 
 echo -e ${OK}"\nYou are all set. You can now define zsh as your default shell using the command :\nchsh -s $(which zsh)"${DEFAULT}
